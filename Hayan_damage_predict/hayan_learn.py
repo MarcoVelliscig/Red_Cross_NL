b56634764cc8a8672c0dd93421e5bfa9a72fa2ed
MarcoVelliscig <MarcoVelliscig@users.noreply.github.com>
2016-10-19 14:11:34 +0200

    new random search, NN

--- Hayan_damage_predict/hayan_learn.py
+++ Hayan_damage_predict/hayan_learn.py
@@ -1,3 +1,21 @@
+# automatic selection of best feautures/ combination of feautures
+# grid parameter search
+# neural network
+
+# Utility function to report best scores
+def report(grid_scores, n_top=5):
+    top_scores = sorted(grid_scores, key=itemgetter(1), reverse=True)[:n_top]
+    for i, score in enumerate(top_scores):
+        print("Model with rank: {0}".format(i + 1))
+        print("Mean validation score: {0:.3f} (std: {1:.3f})".format(
+              score.mean_validation_score,
+              np.std(score.cv_validation_scores)))
+        print 'cross validation scores', score.cv_validation_scores
+        print("Parameters: {0}".format(score.parameters))
+        print("")
+
+
+
 def norm_column(df , name_vec , new_name_vec , norm_type = '0mean1std', rename = False):
     """ Normalize column across differnt train and test dataset  """
     #the min should be the min of the min to avoid negative params
@@ -79,126 +97,83 @@ def one_hot(df , name_vec , drop = False) :
         print df.columns
     return df
 
+def model_pred(X,Y,
+               hyperparams ,
+               maximize='accuracy' ,
+               model_type='logreg' ,
+               n_iter_search = 30,
+               n_cv_sets = 10 ,
+               limits = [-3 , 1 , 0.5] ):
 
 
 
-# X_train, X_test, y_train, y_test = train_test_split(
-#     X, y, test_size=0.5, random_state=0)
+    X_train, X_test , Y_train , Y_test= train_test_split(X,Y, test_size = 0.3)
 
-# # Set the parameters by cross-validation
-# tuned_parameters = [{'kernel': ['rbf'], 'gamma': [1e-3, 1e-4],
-#                      'C': [1, 10, 100, 1000]},
-#                     {'kernel': ['linear'], 'C': [1, 10, 100, 1000]}]
-
-# scores = ['precision', 'recall']
-
-# for score in scores:
-#     print("# Tuning hyper-parameters for %s" % score)
-#     print()
-
-#     clf = GridSearchCV(SVC(C=1), tuned_parameters, cv=5,
-#                        scoring='%s_weighted' % score)
-#     clf.fit(X_train, y_train)
-
-#     print("Best parameters set found on development set:")
-#     print()
-#     print(clf.best_params_)
-#     print()
-#     print("Grid scores on development set:")
-#     print()
-#     for params, mean_score, scores in clf.grid_scores_:
-#         print("%0.3f (+/-%0.03f) for %r"
-#               % (mean_score, scores.std() * 2, params))
-#     print()
-
-#     print("Detailed classification report:")
-#     print()
-#     print("The model is trained on the full development set.")
-#     print("The scores are computed on the full evaluation set.")
-#     print()
-#     y_true, y_pred = y_test, clf.predict(X_test)
-#     print(classification_report(y_true, y_pred))
-#     print()
-
-
-
-
-
-
-
-
-def model_pred(X,Y, maximize='accuracy' , model_type='logreg' ,limits = [-3 , 1 , 0.5] ):
-
-
-    #learn Logistic regression
-    best_lambda = 0
-    best_score = -1
-    n_cv_sets = 5
-    X_train, X_test , Y_train , Y_test= train_test_split(X,Y, test_size = 1/float(n_cv_sets))
-
-    for i in np.arange(limits[0],limits[1] , limits[2]):
-        lambd = 10**i
-        print lambd
-        if model_type=='linreg' :
-            model = linear_model.Ridge(alpha =  lambd )
-        if model_type=='lasso' :
-            model = linear_model.Lasso(alpha =  lambd )
-        elif model_type=='randomforest' :
-            model = RandomForestRegressor(n_estimators =int(lambd), n_jobs=2)
-        elif model_type=='GBR' :
-            model = GradientBoostingRegressor(n_estimators=int(lambd))
-
-        r2 = cross_validation.cross_val_score(model, X_train, Y_train, cv=n_cv_sets, scoring='r2')
-        #print 'accuracy split' , accuracy
-        print("r2: %0.2f (+/- %0.2f)" % (r2.mean(), r2.std() * 2))
-        
-
-        if maximize=='r2': score_to_max = r2.mean()
-
-
-        if  score_to_max > best_score :
-            best_lambda = lambd
-            best_score = score_to_max
-            print 'best score ' , best_score , 'best lambd ' , best_lambda
-
-    print 'done searching'
-
-    print '===========  best score ' , best_score , 'best lambd ' , best_lambda
-    #use the best lambda to fir the model
+    param_dist = hyperparams
+    
     if model_type=='linreg' :
-        model = linear_model.Ridge(alpha =  best_lambda )
-        model.fit(X_train, Y_train)
-        #print  zip(X_train.columns , model.coef_)
-    elif model_type=='lasso' :    
-        model = linear_model.Lasso(alpha =  best_lambda )
-        model.fit(X_train, Y_train)        
+        model = linear_model.ElasticNet()
+    elif model_type=='lasso' :
+        model = linear_model.Lasso()
     elif model_type=='randomforest' :
-        model = RandomForestRegressor(n_estimators =int(best_lambda), n_jobs=2)
-        model.fit(X_train, Y_train)
-        #print zip(X_train.columns , model.feature_importances_)
-    elif model_type=='GBR' :
-        model = GradientBoostingRegressor(n_estimators=int(best_lambda))
-        model.fit(X_train, Y_train)
-    elif model_type=='NN' :
-        model = GradientBoostingClassifier(n_estimators=int(best_lambda), learning_rate=1.0,max_depth=1, random_state=0)
+        model = RandomForestRegressor()
+    elif model_type=='GBT' :
+        model = GradientBoostingRegressor()
+    elif model_type == 'NN':
+        model = MLPRegressor()
 
-        model.fit(X_train, Y_train)
-    fi = np.array(zip(X.columns , model.feature_importances_))
-    print sorted(fi,key=lambda x: x[1])
-    print '===params best model' ,model.get_params()
-    score_test = model.score(X_test, Y_test)
-    print 'score test' ,score_test
+    #how to decide the score
+    random_search = RandomizedSearchCV(model, param_distributions=param_dist,n_iter=n_iter_search, cv = n_cv_sets, )#scoring=
+    #scorer(estimator, X, y)
 
-    #sklearn.cross_validation.cross_val_predict(estimator, X, y=None, cv=None, n_jobs=1, verbose=0, fit_params=None, pre_dispatch='2*n_jobs')[source]
-    Y_test_pred = (model.predict(X_test))
+    start = time()
+    random_search.fit(X_train, Y_train)
+    print("RandomizedSearchCV took %.2f seconds for %d candidates"
+          " parameter settings." % ((time() - start), n_iter_search))
+    report(random_search.grid_scores_)
+    #random_search.fit(X_train, Y_train)
+    Y_test_pred= random_search.predict(X_test)
+    print 'score test set' , random_search.score(X_test, Y_test)
+
+
+
+
+    print '===params best model' ,random_search.best_params_ 
+
+
+
     print ' diff pred and ground ' , np.mean(abs(Y_test_pred -Y_test ))
-    #Y_sub_pred= pd.Series(model.predict(X))
+    
+
+
+    model =random_search.best_estimator_    
     Y_sub_pred=cross_validation.cross_val_predict(model, X, y=Y, cv=n_cv_sets, n_jobs=2)
-    r2 = metrics.r2_score(Y, Y_sub_pred)
-    print 'score full dataset' ,r2
-    fi = np.array(zip(X.columns , model.feature_importances_))
-    print sorted(fi,key=lambda x: x[1])
-    return Y_sub_pred , best_score , model
+    #Y_sub_pred=(model.predict(X))
+    best_score=random_search.best_score_ 
+    print ' score on train set' , best_score
+
+    if model_type=='linreg' :
+        print zip(X_train.columns , model.coef_)
+    elif model_type=='lasso' :
+        model = linear_model.Lasso()
+    elif model_type=='randomforest' :
+        print zip(X_train.columns , model.feature_importances_)
+    elif model_type=='GBT' :
+        print zip(X_train.columns , model.feature_importances_)         
+    elif model_type == 'NN':
+        model = MLPRegressor()
+
+
+    return Y_sub_pred , best_score
+
+
+
+
+
+
+
+
+
 
 
 import numpy as np
@@ -210,7 +185,6 @@ import csv as csv
 import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn import cross_validation
-from sklearn import metrics
 from sklearn.dummy import DummyClassifier
 from sklearn.cross_validation import train_test_split
 from sklearn.metrics import confusion_matrix
@@ -219,8 +193,12 @@ from sklearn.metrics import f1_score
 from sklearn.svm import SVC
 from sklearn.ensemble import GradientBoostingRegressor
 from sklearn.ensemble import RandomForestRegressor
-
 import string
+from time import time
+from scipy.stats import randint as sp_randint
+from operator import itemgetter
+from sklearn.grid_search import GridSearchCV, RandomizedSearchCV
+from sklearn.neural_network import MLPRegressor
 #df = pd.read_excel('Haiyan_Overview.xlsx')
 #df = pd.read_excel('150707_Philippines_MultiplicModel_and_DEA(1I_2O_CCR_O)output_combined.xlsx',sheetname='DataWithDEA')
 
@@ -246,7 +224,7 @@ import string
 new_f= [ 'Perimeter (m)','Coastline length (m)', 'C/P Ratio',
          'Distance to coastline (m, mean)', 'Distance to coastline (m, stdev)',
          'Ruggedness index (mean)', 'Ruggedness index (stdev)','Slope (mean)', 
-         'Slope (stdev)', 'Landuse (Most common)', 'Rainfallme']
+         'Slope (stdev)', 'Landuse (Most common)', 'Rainfallme','Surge_Hint']
 #df[new_f].hist()
 #plt.show()
 #df = pd.read_csv('Haiyan_Overview.csv')
@@ -258,12 +236,10 @@ for col in df.columns : print df.loc[df['People affected']>350000,col]
 len(df[df['People affected (% 2010)']==0.5])
 
 
-
 df['Coastline length (m)'] = df['Coastline length (m)'] +1.0
 
 #----------------------------
 df['poverty_frac'] = df['Poverty (%)']/100.
-
 
 transformation = 'log_div_max_log'
 print  'transform as ' , transformation , '\n'
@@ -310,7 +286,7 @@ name_new=  ['dist_norm',
             'rugged_index',
             'rugged_index_std',
             'slope_mean', 
-            'slope_std',
+            'slope_std',	
             'rainfall']
 
 print  'varaiables to transform ' ,  '\n' , zip(name_old , name_new)
@@ -332,26 +308,27 @@ df = df.dropna(subset=[ 'surge_height_log','pop10_log','perc_people_affected','p
 
 
 
-improved_feauture_list = [#'area_log' , 
-                          #'elevation_log', 
+improved_feauture_list = ['area_log' , 
+                          'elevation_log', 
                           'poverty_frac', 
                           'pop10_log',
                           'pop_density_log' ,
                           'wind_speed_log', 
                           'dist_norm', 
                           'surge_height_log', 
+                          'C/P Ratio',
                           'rugged_index',
-                          #'coastline_length',
+                          'coastline_length',
                           'dist_coast',
-                          #'dist_coast_std',
-                          #'rugged_index_std',
-                          #'slope_mean', 
-                          #'slope_std', 
+                          'dist_coast_std',
+                          'rugged_index_std',
+                          'slope_mean', 
+                          'slope_std', 
                           'rainfall',
-                          #'C/P Ratio'
                           #14, 20, 40,130,190, 210
 ]
 
+#improved_feauture_list = ['dist_norm']
 
 
 
@@ -362,45 +339,102 @@ improved_feauture_list = [#'area_log' ,
 
 df = df.dropna(subset=[ 'surge_height_log','pop10_log','perc_people_affected','poverty_frac' ,'Houses damaged (REACH)']).reset_index()
 
+#df = df.loc[df['Houses damaged (REACH)']>10]
+
 X = df[improved_feauture_list]
+ids = df['M_Code']
+
 X.hist()
 plt.show()
 
+
+#=============================
+
 predict_on = 'Houses damaged (REACH)'
-Y = df[predict_on]/df['Population 2010 census']
+Y = np.log(df[predict_on]/df['Population 2010 census'])
+
+
+
+hyperparams_dict ={}
+hyperparams_dict['GBT'] = {"loss" : ['ls', 'lad', 'huber'],
+                           "n_estimators" : [100,500,700,1000],
+                           "learning_rate": [0.01, 0.05, 0.1, 0.2],
+                           "max_depth": [3, 2,None],
+                           'max_features': ['auto', 'sqrt', 'log2'],
+                           "min_samples_split": sp_randint(1, 11),
+                           "min_samples_leaf": sp_randint(1, 11),
+                           "criterion": ["mse", "friedman_mse"]}
+
+
+
+hyperparams_dict['linreg'] = {"alpha" : 10.0 ** np.arange(-4, 1, 0.2),
+                              "l1_ratio": np.arange(0. , 1. , 0.1),
+                              "fit_intercept":[False]}
+
+hyperparams_dict['NN']= {"hidden_layer_sizes":[(5, ),(10, ),(5,3 ),(10,3),(200, 10),(500, 100,20)],
+                         "activation" : ['identity', 'logistic', 'tanh', 'relu'],
+                         "alpha" : 10.0 ** -np.arange(1, 7),
+                         "max_iter" :  [int(10**x) for x in np.arange(2, 4,0.5)]}
+
+hyperparams_dict['randomforest']= {"n_estimators" : [10,50,100,500,700,1000],
+                                   "max_depth": [3, 2, 4 ,None],
+                                   'max_features': ['auto', 'sqrt', 'log2'],
+                                   "min_samples_split": sp_randint(1, 11),
+                                   "min_samples_leaf": sp_randint(1, 11),
+                                   "bootstrap": [True, False],
+                                   "criterion": ["mse"]}
+
+
+models_vector = [ 'linreg', 'NN', 'GBT', 'randomforest']
+
+for model_name in models_vector:
+
+#====================
+
+    hyperparams = hyperparams_dict[model_name]
+
+
+
+    Y_pred , best = model_pred(X,Y,hyperparams , model_type=model_name , n_iter_search = 40, n_cv_sets = 10 )
+    print model_name , ' mean house damage  error' , np.mean(abs(np.exp(Y_pred) - np.exp(Y))*df['Population 2010 census'])
+    print model_name , ' median house damage  error' , np.median(abs(np.exp(Y_pred) - np.exp(Y))*df['Population 2010 census'])
+#====================
+
+
+
+    predict_on_tag = 'houses'
+    Y_df_h = pd.DataFrame(np.exp(Y.values) )
+    Y_df_h.columns = ['perc_'+predict_on_tag+'_true'] 
+    Y_df_h['perc_'+predict_on_tag+'_pred'] = np.exp(Y_pred)
+    Y_df_h['M_Code'] = df['M_Code']
+    Y_df_h['Municipality'] = df['Municipality']
+    Y_df_h['num_'+predict_on_tag+'_pred'] = Y_df_h['perc_'+predict_on_tag+'_pred']*df['Population 2010 census']
+    Y_df_h['num_'+predict_on_tag+'_true'] = Y_df_h['perc_'+predict_on_tag+'_true']*df['Population 2010 census']
+    Y_df_h['num_'+predict_on_tag+'_error'] = abs(Y_df_h['num_'+predict_on_tag+'_pred'] - Y_df_h['num_'+predict_on_tag+'_true'])
+    Y_df_h['num_'+predict_on_tag+'_error_noabs'] = (Y_df_h['num_'+predict_on_tag+'_pred'] - Y_df_h['num_'+predict_on_tag+'_true'])
+
+    Y_df_h['perc_'+predict_on_tag+'_error'] = abs(Y_df_h['perc_'+predict_on_tag+'_pred'] - Y_df_h['perc_'+predict_on_tag+'_true'])
+    Y_df_h['rel_num_'+predict_on_tag+'_error'] = Y_df_h['num_'+predict_on_tag+'_error'] / Y_df_h['num_'+predict_on_tag+'_true'] 
+
+    Y_df_h_sort = Y_df_h.sort_values('num_houses_true', axis=0)
+    Y_df_h_sort = Y_df_h_sort.reset_index()
+
+    Y_df_h_sort['num_houses_true'].plot()
+    Y_df_h_sort['num_houses_pred'].plot()
+    Y_df_h_sort['num_houses_error_noabs'].plot()
+    plt.show()
+     
+    Y_df_h_sort['num_houses_error_noabs'].hist()
+    plt.show()
+
+    Y_df_h.to_csv("house_damaged_pred_" + model_name + ".csv")
 
 
 
 
-model_name='randomforest'
-#model_name='GBR'
-Y_pred_randomforest , best , model_rf = model_pred(X,Y, maximize='r2' , model_type=model_name ,limits = [1 , 3.5 , 0.3] )
-print model_name , 'mean house damage error' , np.mean(abs(Y_pred_randomforest - Y)*df['Population 2010 census'])
-#print 'randomforest mean pop affected error' , np.mean(abs(Y_pred_randomforest - Y))*df['People affected'].mean()
-
-predict_on_tag = 'houses'
-Y_df_h = pd.DataFrame(Y.values )
-Y_df_h.columns = ['perc_'+predict_on_tag+'_true'] 
-Y_df_h['perc_'+predict_on_tag+'_pred'] = Y_pred_randomforest
-Y_df_h['M_Code'] = df['M_Code']
-Y_df_h['Municipality'] = df['Municipality']
-Y_df_h['num_'+predict_on_tag+'_pred'] = Y_df_h['perc_'+predict_on_tag+'_pred']*df['Population 2010 census']
-Y_df_h['num_'+predict_on_tag+'_true'] = Y_df_h['perc_'+predict_on_tag+'_true']*df['Population 2010 census']
-Y_df_h['num_'+predict_on_tag+'_error'] = abs(Y_df_h['num_'+predict_on_tag+'_pred'] - Y_df_h['num_'+predict_on_tag+'_true'])
-Y_df_h['perc_'+predict_on_tag+'_error'] = abs(Y_df_h['perc_'+predict_on_tag+'_pred'] - Y_df_h['perc_'+predict_on_tag+'_true'])
-Y_df_h['rel_num_'+predict_on_tag+'_error'] = Y_df_h['num_'+predict_on_tag+'_error'] / Y_df_h['num_'+predict_on_tag+'_true'] 
 
 
 
 
 
-print Y_df_h.describe()
-
-Y_df_h.to_csv("house_d_29062016.csv")
-
-
-Y_df_h[[u'num_houses_pred',u'num_houses_true',]].plot()
-Y_df_h['num_houses_pred'].hist(bins=20,alpha=0.5)
-Y_df_h['num_houses_true'].hist(bins=30,alpha=0.5)
-plt.show()
 
